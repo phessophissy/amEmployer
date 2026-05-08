ceca30b073907c04839f58f8cf1bcde5031da0ea
phessophissy <phessophissy@users.noreply.github.com>
2026-05-08 21:48:52 +0100

    chore(employer): export additional employer components from barrel

--- packages/frontend/src/components/employer/index.ts
+++ packages/frontend/src/components/employer/index.ts
@@ -3,3 +3,6 @@ export { StepIndicator } from './StepIndicator';
 export { JobBasicsStep } from './JobBasicsStep';
 export { BudgetStep } from './BudgetStep';
 export { ReviewStep } from './ReviewStep';
+export { JobStatusPill } from './JobStatusPill';
+export { BudgetBar } from './BudgetBar';
+export { TaskCountBadges } from './TaskCountBadges';
