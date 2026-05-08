2934fea5aae72d0ef797acfc7a540f3e0984142c
phessophissy <phessophissy@users.noreply.github.com>
2026-05-08 21:49:24 +0100

    chore(simulation): export TaskPipelineViz from barrel

--- packages/frontend/src/components/simulation/index.ts
+++ packages/frontend/src/components/simulation/index.ts
@@ -2,3 +2,4 @@ export { SimulationControls } from './SimulationControls';
 export { LiveActivityStream } from './LiveActivityStream';
 export { WorkerHeatmap } from './WorkerHeatmap';
 export { SimulationMetrics } from './SimulationMetrics';
+export { TaskPipelineViz } from './TaskPipelineViz';
