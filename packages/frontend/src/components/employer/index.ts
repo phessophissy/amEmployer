134d676836071058db04d019b71fd068f8c2ecaa
phessophissy <phessophissy@users.noreply.github.com>
2026-05-08 21:49:41 +0100

    chore(employer): export drawer and new employer components from barrel

---
+++ packages/frontend/src/components/employer/index.ts
@@ -0,0 +1,8 @@
+export { JobDetailDrawer } from './JobDetailDrawer';
+export { TaskList } from './TaskList';
+export { JobCard } from './JobCard';
+export { QuickStats } from './QuickStats';
+export { CreateJobFAB } from './CreateJobFAB';
+export { EmptyJobsState } from './EmptyJobsState';
+export { PaymentFeed } from './PaymentFeed';
+export { AILogSidebar } from './AILogSidebar';
