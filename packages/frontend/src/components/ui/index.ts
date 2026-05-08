8582fe7bc765640a1bbb807372f30e32e58be4ce
phessophissy <phessophissy@users.noreply.github.com>
2026-05-08 21:48:46 +0100

    chore(ui): export barrel for UI components

---
+++ packages/frontend/src/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { ToastContainer } from './Toast';
+export { ConfirmDialog } from './ConfirmDialog';
+export { LoadingSpinner } from './LoadingSpinner';
+export { Badge } from './Badge';
+export { ProgressBar } from './ProgressBar';
+export { Skeleton, CardSkeleton } from './Skeleton';
