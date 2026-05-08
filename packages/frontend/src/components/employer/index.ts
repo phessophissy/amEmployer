18e3606a1a702b9cf52bb4c442e243671a801681
phessophissy <phessophissy@users.noreply.github.com>
2026-05-08 21:48:52 +0100

    chore(employer): export barrel for employer components

---
+++ packages/frontend/src/components/employer/index.ts
@@ -0,0 +1,5 @@
+export { CreateJobStepper } from './CreateJobStepper';
+export { StepIndicator } from './StepIndicator';
+export { JobBasicsStep } from './JobBasicsStep';
+export { BudgetStep } from './BudgetStep';
+export { ReviewStep } from './ReviewStep';
